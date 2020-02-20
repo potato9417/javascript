34cb8e7e651c30c9eda6f691399d44af941595a9
potato9417 <potato9417@gmail.com>
2020-02-20 18:09:16 +0900

    20200220-c

--- dongah/js/main.js
+++ dongah/js/main.js
@@ -27,6 +27,22 @@ $("#lang a").on("click",function(){
 })
 
 new fullpage("#main", {
+    scrollOverflow: true,
+    onLeave: function(orgin,destination){
+        let idx = destination.index;
+        if(idx===0){
+            $("#header").removeClass("tl");
+        }
+        else {
+            $("#header").addClass("tl");
+        }
+    }
 });
+
+
+// fullpage에서 원하는 페이지를 찾을때 onLeave: function(orgin,destination){}
+// orgin,destination은 변수이고 destination이 섹션의 위치를 알수있게 해줌
+// console.log(destination); 중에 index가 위치
+// let idx = destination.index; 를 변수로 새로 잡아서 함
 
 
