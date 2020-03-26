c096c4872cba71fa9ab1ddb15c6d1c78f8c1972b
potato9417 <potato9417@gmail.com>
2020-03-26 18:06:32 +0900

    20200326-t

--- shinhan/js/main.js
+++ shinhan/js/main.js
@@ -1,15 +1,34 @@
 new fullpage("#fullpage", {
+	navigation: true,
+	navigationPosition: "left",
+	navigationTooltips: ["COMPANY", "BUSINESS", "PR CENTER", "RECRUIT"],
+	showActiveTooltip: true,
 });
 
 $("#gnb .gnbList > li").on("mouseenter",function(){
     let index = $(this).index();
     console.log(index);
     $("#header").addClass("on");
-    if(index===1){
-        
+    $(this).find("ul").addClass("on");
+    $(this).siblings().find("ul").removeClass("on");
+    if(!$(this).hasClass("on")){
+        $(this).addClass("on");
+        $(this).siblings().removeClass("on");
     }
+})
+$("#gnb .gnbList > li").on("mouseleave",function(){
+    $("#header").removeClass("on");
 })
 
 $(".util .btns").on("click",function(){
     $("#header").toggleClass("click");
+})
+
+$("#mainVisual .moreBox .btnsMore .moreOpen").on("click",function(){
+    $("#mainVisual .moreBox").addClass("open");
+    return false;
+})
+$("#mainVisual .moreBox .btnsMore .moreClose").on("click",function(){
+    $("#mainVisual .moreBox").removeClass("open");
+    return false;
 })
