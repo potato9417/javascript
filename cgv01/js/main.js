e6d72bf8a4bf8dfd098ddf923e8d7d24abb03584
potato9417 <potato9417@gmail.com>
2020-04-30 13:40:36 +0900

    2020-04-30

--- cgv01/js/main.js
+++ cgv01/js/main.js
@@ -1,12 +1,36 @@
 
-const xAxis = 16;
-const yAxis = 13;
+const xAxis = 16,
+    yAxis = 13,
+    seats = document.getElementById("lines"),
+    seatLine = [];
+    
+    
+    
+
+let seatArr = [];
 
 for(let y = 0; y < yAxis; y++) {
     for (let x = 0; x < xAxis; x ++) {
-        console.log(`좌표>>>${x},${y}`);
+        let seatLine = ["A","B","C","D","E","F","G","H","I","J","K","L","M"];
+        seatArr.push({xSeat:x,ySeat:seatLine[Math.floor(y%13)]});
+        // console.log(seatArr);
+        // 배열반복을 끝냈을 때 실행하도록 만든 if문
+        if(seatArr.length===xAxis*yAxis){
+            for(let i in seatArr){
+                // p태그를 append하는 방법(https://stackoverflow.com/questions/44288505/passing-in-array-data-into-div-elements)
+                const p = document.createElement("p");
+                p.append(`${seatArr[i].ySeat}${seatArr[i].xSeat}`);
+                console.log(seats);
+                seats.append(p);
+                // append 문자열추가(원래있던것에 추가)
+                // innerHtml 문자열추가(원래있던것을 지우고)
+                // appendChild tag생성("p")
+            }
+        }
+        
     }
 }
+
 
 
 
@@ -14,27 +38,7 @@ for(let y = 0; y < yAxis; y++) {
 //     selectSeat = document.getElementById("selectSeat"),
 //     // selectSeat = document.querySelector("#selectSeat"),
 //     selectPrice = document.getElementById("selectPrice"),
-//     seatArr = [
-//         {
-//             seat:["A","B","C","D","E","F","G","H","I","J","K","L","M"], 
-//             num:[
-//                 1,2,3,4,5,6,7,8,9,10,13,14,
-//                 1,2,3,4,5,6,7,8,9,10,11,12,13,14,
-//                 1,2,3,4,5,6,7,8,9,10,11,12,13,14,
-//                 1,2,3,4,5,6,7,8,9,10,11,12,13,14,
-//                 1,2,3,4,5,6,7,8,9,10,11,12,13,14,
-//                 1,2,3,4,5,6,7,8,9,10,11,12,13,14,
-//                 3,4,5,6,7,8,9,10,11,12,13,14,
-//                 3,4,5,6,7,8,9,10,11,12,13,14,
-//                 3,4,5,6,7,8,9,10,11,12,13,14,
-//                 3,4,5,6,7,8,9,10,11,12,13,14,
-//                 3,4,5,6,7,8,9,10,11,12,13,14,15,16,
-//                 3,4,5,6,7,8,9,10,11,12,13,14,15,16,
-//                 3,4,5,6,7,8,9,10,11,12,13,14,15,16,
-//             ]
-//         },
-//     ]
-    
+
     
 
 // let price = 0,
